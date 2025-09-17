047deccb715a63580435ebb7df4fec07ac3849de
Solingaram Dhruva Narayan <131137542+dhruvanarayan@users.noreply.github.com>
2025-09-17 20:40:29 +0530

    47146

--- home/models.py
+++ home/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 
+# Create your models here.
 class MenuCategory(models.Model):
-    """
-    Represents categories for restaurant menu items like
-    'Appetizers', 'Main Courses', 'Desserts'.
-    """
-    name = models.CharField(max_length=100, unique=True)
-
+    name =models.CharField(max_length = 100, unique = True)
+    
     class Meta:
         verbose_name_plural = "Menu Categories"
 
     def __str__(self):
-        return self.name
+        return self.name 
